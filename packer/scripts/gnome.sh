3cb84cc7458ee812ca3523d0dde690aa9cec3916
sven <sven@so36.net>
2016-07-15 14:27:23 +0200

    Wip: Some little fixes in script

--- packer/scripts/gnome.sh
+++ packer/scripts/gnome.sh
@@ -4,20 +4,21 @@
 wget http://dist.tiramisu.solutions/files.zip -O /tmp/files.zip 
 
 # Unzip /tmp/files.zip
-unzip /tmp/files.zip -d /tmp/files
+unzip /tmp/files.zip -d /tmp/
 
 # Install 'paper' theme and icons
 dpkg -i /tmp/files/paper-gtk-theme_2.1+r263~daily~ubuntu16.04.1_all.deb
 dpkg -i /tmp/files/paper-icon-theme_1.3+r567~daily~ubuntu16.04.1_all.deb
 
 # Install gnome-extensions
-./tmp/files/gnome-shell-extension-installer 2 3.14 --yes   # Frippery move clock
-./tmp/files/gnome-shell-extension-installer 941 3.14 --yes # Show IP extension
+/tmp/files/gnome-shell-extension-installer 2 3.14 --yes   # Frippery move clock
+/tmp/files/gnome-shell-extension-installer 941 3.14 --yes # Show IP extension
 
 # Set Gnome-theme
 cd /home/vagrant
-sudo vagrant -u gsettings set org.gnome.desktop.interface gtk-theme 'Paper'
-sudo vagrant -u gsettings set org.gnome.desktop.interface icon-theme 'Paper'
+sudo -u vagrant gsettings set org.gnome.desktop.wm.preferences theme "Paper"
+sudo -u vagrant gsettings set org.gnome.desktop.interface gtk-theme 'Paper'
+sudo -u vagrant gsettings set org.gnome.desktop.interface icon-theme 'Paper'
 
 # Set Gnome Fonts
 sudo -u vagrant gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Cantarell Bold 10'
@@ -31,7 +32,8 @@ sudo -u vagrant gsettings set org.gnome.desktop.background secondary-color "#425
 sudo -u vagrant gsettings set org.gnome.desktop.background color-shading-type "solid"
 sudo -u vagrant gsettings set org.gnome.desktop.background picture-options "none"
 
-
+# Restart Gnome
+gnome-shell --replace
 
 
 

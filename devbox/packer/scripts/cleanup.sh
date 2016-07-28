677a4640398a1db2441beb8dafe4bce4c243fb9b
sven <sven@so36.net>
2016-07-28 13:49:01 +0200

    Cleanup: Repo

--- devbox/packer/scripts/cleanup.sh
+++ devbox/packer/scripts/cleanup.sh
@@ -12,9 +12,21 @@ echo 'iface eth0 inet dhcp' >> /etc/network/interfaces
 echo 'auto eth1' >> /etc/network/interfaces
 echo 'iface eth1 inet manual' >> /etc/network/interfaces
 
+
+# delete oddities
+apt-get -y purge popularity-contest
+
+# Remove unused locales
+echo "Removing unused locales"
+rm -rf /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,pt,pt_BR,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu}
+
+# Remove files from cache
+echo "Removing cache"
+find /var/cache -type f -delete -print
+
 # Apt cleanup.
-apt autoremove
-apt update
+apt-get -y --purge autoremove
+apt-get -y autoclean
 
 # Delete unneeded files.
 rm -f /home/vagrant/*.sh
@@ -22,6 +34,9 @@ rm -f /home/vagrant/*.sh
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M || true
 rm -f /EMPTY
+
+# Remove history file
+unset HISTFILE
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
 sync
